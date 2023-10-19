dfc71d6c4c5002b6bf4e85e73a682ef39efc77e5
XH <xh_sxl@163.com>
2023-10-19 20:09:06 +0800

    update

--- MyLeetCode/JavaCodes/src/codes/arry/ArrayStudy.java
+++
@@ -1,8 +0,0 @@
-package codes.arry;
-
-public class ArrayStudy {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Java");
-    }
-}
