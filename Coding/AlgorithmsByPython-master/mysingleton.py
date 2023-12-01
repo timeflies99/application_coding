e584cff077b7567f9312bbdb21a5791e7d1d3e01
timeflies99 <37474646+timeflies99@users.noreply.github.com>
2023-12-01 11:01:18 +0800

    Delete Coding directory

--- Coding/AlgorithmsByPython-master/mysingleton.py
+++
@@ -1,5 +0,0 @@
-# 单例模式
-class My_Singleton(object):
-    def foo(self):
-        pass
-my_singleton = My_Singleton()
--- Coding/Offer/offer1/044-翻转单词顺序列/code.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def ReverseSentence(self, s):
-        # write code here
-        stack = [n for n in s.split(' ')]
-        stack.reverse()
-        return ' '.join(stack)
-
-print(Solution().ReverseSentence("I am a student."))
